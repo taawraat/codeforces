67b89d5ec6ec94acdaa7c1e603aa56b4a8decd97
Tawraat <tawraat0187@gmail.com>
2020-09-09 14:11:10 +0600

    accepted Sep/08/2020

--- 977A - Wrong Subtraction.cpp
+++ 977A - Wrong Subtraction.cpp
@@ -1,18 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-  int num; cin >> num;
-
-  int sub; cin >> sub;
-
+  int num, sub; cin >> num >> sub;
+  
   while(sub--)
-  {
-    if(num % 10 == 0)
-      num /= 10;
-    else
-      num--;
-  }
+    num % 10 ? num-- : num /= 10;
+
   cout << num << endl;
 }
